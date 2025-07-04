3d02a8d45710e8dbdca105d48f29c75d12ff9f0a
慕寒吖 <767397654@qq.com>
2025-07-04 21:41:04 +0800

    1

---
+++ DesktopPet/TransparentDrawingView.swift
@@ -0,0 +1,18 @@
+//
+//  TransparentDrawingView.swift
+//  DesktopPet
+//
+//  Created by 李瑞华 on 2025/7/4.
+//
+
+import Cocoa
+
+class TransparentDrawingView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
