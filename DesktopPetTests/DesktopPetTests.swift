da809820d15a7da3783221259182e6cde951d6b5
慕寒吖 <767397654@qq.com>
2025-07-04 16:21:10 +0800

    Initial Commit

---
+++ DesktopPetTests/DesktopPetTests.swift
@@ -0,0 +1,17 @@
+//
+//  DesktopPetTests.swift
+//  DesktopPetTests
+//
+//  Created by 李瑞华 on 2025/7/4.
+//
+
+import Testing
+@testable import DesktopPet
+
+struct DesktopPetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
